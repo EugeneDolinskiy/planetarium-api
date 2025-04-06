16b91dda2e1adfc711808a3cddbd183c1d639a6f
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-06 20:35:59 +0300

    register PlanetariumDome model in admin.py

--- cosmoshow/admin.py
+++ cosmoshow/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from cosmoshow.models import ShowTheme, AstronomyShow
+from cosmoshow.models import ShowTheme, AstronomyShow, PlanetariumDome
 
 admin.site.register(ShowTheme)
 admin.site.register(AstronomyShow)
+admin.site.register(PlanetariumDome)
