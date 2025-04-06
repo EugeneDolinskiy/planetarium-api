6eb793c355a03309cafe7c6a81e7351b18aa91f6
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-06 21:14:41 +0300

    add route to show-session

--- cosmoshow/urls.py
+++ cosmoshow/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from cosmoshow.views import ShowThemeViewSet, AstronomyShowViewSet, PlanetariumDomeViewSet
+from cosmoshow.views import ShowThemeViewSet, AstronomyShowViewSet, PlanetariumDomeViewSet, ShowSessionViewSet
 
 router = routers.DefaultRouter()
 router.register("show-themes", ShowThemeViewSet)
 router.register("astronomy-shows", AstronomyShowViewSet)
 router.register("planetarium-domes", PlanetariumDomeViewSet)
+router.register("show-session", ShowSessionViewSet)
 
 urlpatterns = [path('', include(router.urls))]
 
