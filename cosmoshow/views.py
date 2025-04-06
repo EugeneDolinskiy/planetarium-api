f3240b7fee5e5dc4670374f47699f3e84e2f2b26
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-06 15:50:39 +0300

    add AstronomyShowViewSet to views.py

--- cosmoshow/views.py
+++ cosmoshow/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from cosmoshow.models import ShowTheme
-from cosmoshow.serializers import ShowThemeSerializer
+from cosmoshow.models import ShowTheme, AstronomyShow
+from cosmoshow.serializers import ShowThemeSerializer, AstronomyShowSerializer
 
 
 class ShowThemeViewSet(viewsets.ModelViewSet):
     queryset = ShowTheme.objects.all()
     serializer_class = ShowThemeSerializer
+
+
+class AstronomyShowViewSet(viewsets.ModelViewSet):
+    queryset = AstronomyShow.objects.all()
+    serializer_class = AstronomyShowSerializer
