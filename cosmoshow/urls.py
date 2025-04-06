9e4aee896c71f30f26f0f122efe272f89b9fd2d1
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-06 20:40:29 +0300

    add route to planetarium-domes

--- cosmoshow/urls.py
+++ cosmoshow/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from cosmoshow.views import ShowThemeViewSet, AstronomyShowViewSet
+from cosmoshow.views import ShowThemeViewSet, AstronomyShowViewSet, PlanetariumDomeViewSet
 
 router = routers.DefaultRouter()
 router.register("show-themes", ShowThemeViewSet)
 router.register("astronomy-shows", AstronomyShowViewSet)
+router.register("planetarium-domes", PlanetariumDomeViewSet)
 
 urlpatterns = [path('', include(router.urls))]
 
