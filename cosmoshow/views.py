d0bf2f1443a40778e8ddb7f4196a9aa9ac8ea76c
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-06 15:31:47 +0300

    add ShowThemeViewSet to views.py

--- cosmoshow/views.py
+++ cosmoshow/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from cosmoshow.models import ShowTheme
+from cosmoshow.serializers import ShowThemeSerializer
+
+
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
