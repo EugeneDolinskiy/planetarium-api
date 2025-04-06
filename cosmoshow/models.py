f3dd2b8f0dc3460a85a54c9dddc4cef83db1cc71
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-06 20:35:17 +0300

    add PlanetariumDome model to models.py

--- cosmoshow/models.py
+++ cosmoshow/models.py
@@ -15,3 +15,12 @@ class AstronomyShow(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
