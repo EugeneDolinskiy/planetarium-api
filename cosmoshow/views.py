0666a77e8e95437e4bdbc29d996cf7cc86c00951
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-06 21:23:40 +0300

    add ReservationViewSet to views.py

--- cosmoshow/views.py
+++ cosmoshow/views.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
 
-from cosmoshow.models import ShowTheme, AstronomyShow, PlanetariumDome, ShowSession
+from cosmoshow.models import ShowTheme, AstronomyShow, PlanetariumDome, ShowSession, Reservation
 from cosmoshow.serializers import ShowThemeSerializer, AstronomyShowSerializer, AstronomyShowListSerializer, \
     AstronomyShowRetrieveSerializer, PlanetariumDomeSerializer, ShowSessionListSerializer, \
-    ShowSessionRetrieveSerializer, ShowSessionSerializer
+    ShowSessionRetrieveSerializer, ShowSessionSerializer, ReservationSerializer
 
 
 class ShowThemeViewSet(viewsets.ModelViewSet):
@@ -46,3 +46,8 @@ class ShowSessionViewSet(viewsets.ModelViewSet):
         queryset = self.queryset
         if self.action in ("list", "retrieve"):
             return queryset.select_related("astronomy_show", "planetarium_dome")
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
