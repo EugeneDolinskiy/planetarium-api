f4b59f76b25ccc5aa9cb4c5fbdb3cb94e5a861ea
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-06 21:24:22 +0300

    add route to reservations

--- cosmoshow/urls.py
+++ cosmoshow/urls.py
@@ -1,13 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from cosmoshow.views import ShowThemeViewSet, AstronomyShowViewSet, PlanetariumDomeViewSet, ShowSessionViewSet
+from cosmoshow.views import ShowThemeViewSet, AstronomyShowViewSet, PlanetariumDomeViewSet, ShowSessionViewSet, \
+    ReservationViewSet
 
 router = routers.DefaultRouter()
 router.register("show-themes", ShowThemeViewSet)
 router.register("astronomy-shows", AstronomyShowViewSet)
 router.register("planetarium-domes", PlanetariumDomeViewSet)
 router.register("show-session", ShowSessionViewSet)
+router.register("reservations", ReservationViewSet)
 
 urlpatterns = [path('', include(router.urls))]
 
