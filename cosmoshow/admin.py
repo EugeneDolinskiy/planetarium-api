3f7b30efb7d65c6d9162b5f7791eed3132302eb7
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-06 21:21:32 +0300

    register Reservation model in admin.py

--- cosmoshow/admin.py
+++ cosmoshow/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from cosmoshow.models import ShowTheme, AstronomyShow, PlanetariumDome, ShowSession
+from cosmoshow.models import ShowTheme, AstronomyShow, PlanetariumDome, ShowSession, Reservation
 
 admin.site.register(ShowTheme)
 admin.site.register(AstronomyShow)
 admin.site.register(PlanetariumDome)
 admin.site.register(ShowSession)
+admin.site.register(Reservation)
