94a7715a21ed379459144beb27a8a0e6985dab19
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-06 21:20:54 +0300

    add Reservation model to models.py

--- cosmoshow/models.py
+++ cosmoshow/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -41,3 +42,11 @@ class ShowSession(models.Model):
 
     def __str__(self):
         return f"{self.astronomy_show}({self.planetarium_dome}) start at {self.show_time}"
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.created_at
