a4ee4b9ad0f3a05b4458da8885942a335e02af9a
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-04-08 17:19:44 +0300

    remove ReservationViewSet

--- cosmoshow/views.py
+++ cosmoshow/views.py
@@ -1,10 +1,8 @@
 from django.db.models import F, Count
 from rest_framework import viewsets
 
-from cosmoshow.models import ShowTheme, AstronomyShow, PlanetariumDome, ShowSession, Reservation
-from cosmoshow.serializers import ShowThemeSerializer, AstronomyShowBaseSerializer, AstronomyShowListSerializer, \
-    AstronomyShowRetrieveSerializer, PlanetariumDomeBaseSerializer, ShowSessionListSerializer, \
-    ShowSessionRetrieveSerializer, ShowSessionSerializer, ReservationSerializer
+from cosmoshow.models import ShowTheme, AstronomyShow, PlanetariumDome, ShowSession
+from cosmoshow.serializers import ShowThemeSerializer, AstronomyShowBaseSerializer, AstronomyShowListSerializer, AstronomyShowRetrieveSerializer, PlanetariumDomeBaseSerializer, ShowSessionListSerializer, ShowSessionRetrieveSerializer, ShowSessionSerializer
 
 
 class ShowThemeViewSet(viewsets.ModelViewSet):
@@ -51,28 +49,3 @@ class ShowSessionViewSet(viewsets.ModelViewSet):
         elif self.action == "retrieve":
             return ShowSessionRetrieveSerializer
         return ShowSessionSerializer
-
-    def get_queryset(self):
-        queryset = self.queryset
-        if self.action in ("list", "retrieve"):
-            return queryset.select_related("astronomy_show", "planetarium_dome")
-
-
-class ReservationViewSet(viewsets.ModelViewSet):
-    queryset = Reservation.objects.all()
-
-    def get_queryset(self):
-        queryset = self.queryset.filter(user=self.request.user)
-        if self.action == "list":
-            queryset = queryset.prefetch_related("tickets__show_session")
-        return queryset
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
-    def get_serializer_class(self):
-        if self.action == "list":
-            return ReservationListSerializer
-        elif self.action == "retrieve":
-            return ReservationRetrieveSerializer
-        return ReservationSerializer
