e97f27c114308ec9df5cfb80d40eab531cd05405
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-05-09 14:38:41 +0300

    add permission class to RegisterView

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
 from .serializers import RegisterSerializer
 from django.contrib.auth import get_user_model
 
@@ -8,3 +10,4 @@ User = get_user_model()
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
