41270d3c0e5295e959a666ebd3281d2f36bc8f5f
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-05-15 19:54:17 +0300

    load SECRET_KEY from environment variables

--- planetarium/settings.py
+++ planetarium/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%shxz5lw*58pr%d*b%w5+=02!#d+xo2eep$a895k$en%counp8"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
